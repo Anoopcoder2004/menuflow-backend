80031ad8a62b09a2caaaa06c9ef7b16e666a9ecc
Anoop <Anooppopziyan@gmail.com>
2026-01-16 21:06:12 +0530

    first commit

---
+++ src/main/java/com/example/MenuFlow/MenuFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.MenuFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MenuFlow/MenuFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MenuFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
